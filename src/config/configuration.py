c5086f4ab9ab40df80b928d38712d6ad132b7314
Subinoy Bera <subinoyberadgp@gmail.com>
2025-06-08 06:44:44 +0530

    data validation configuration added

--- src/config/configuration.py
+++ src/config/configuration.py
@@ -3,8 +3,8 @@ import sys
 from src.logger import logging
 from src.exception import AppException
 from src.utils import read_yaml, create_directories
-from src.entity.config_entity import DataIngestionConfig
 from constants import *
+from src.entity.config_entity import DataIngestionConfig, DataValidationConfig
 
 class AppConfiguration:
     def __init__(self, config_filepath: str = CONFIG_FILE_PATH):
@@ -48,4 +48,32 @@ class AppConfiguration:
 
         except Exception as e:
             error_message = f"Error in Data Ingestion Configuration: {e}"
+            raise AppException(Exception(error_message), sys)
+
+
+    def data_validation_config(self) -> DataValidationConfig:
+        try:
+            validation_config = self.config.data_validation
+            ingestion_config = self.config.data_ingestion
+            create_directories([validation_config.root_dir])
+
+            validation_root_dir = validation_config.root_dir
+
+            books_csvfile_dir = os.path.join(ingestion_config.root_dir, ingestion_config.ingested_data)
+            ratings_csvfile_dir = os.path.join(ingestion_config.root_dir, ingestion_config.ingested_data)
+            clean_data_path = os.path.join(validation_root_dir, validation_config.clean_data_dir)
+            serialized_obj_dir = os.path.join(validation_root_dir, validation_config.serialized_obj_dir)
+
+            configuration = DataValidationConfig(
+                clean_data_dir = clean_data_path,
+                books_csvfile = books_csvfile_dir,
+                ratings_csvfile = ratings_csvfile_dir,
+                serialized_objects_dir = serialized_obj_dir
+            )
+
+            logging.info(f"Data Validation process Configuration successfull")
+            return configuration
+
+        except Exception as e:
+            error_message = f"Error in Data Validation process Configuration: {e}"
             raise AppException(Exception(error_message), sys)
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -3,3 +3,9 @@ from collections import namedtuple
 DataIngestionConfig = namedtuple("DatasetConfig", ["data_download_url",
                                                    "raw_data_dir",
                                                    "ingested_dir"])
+
+
+DataValidationConfig = namedtuple("DataValidation", ["clean_data_dir",
+                                                    "books_csvfile",
+                                                     "ratings_csvfile",
+                                                     "serialized_objects_dir"])
