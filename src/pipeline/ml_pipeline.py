50a5caf8339a4c15f94ccbe42793a2db1d0a3d2e
Subinoy Bera <subinoyberadgp@gmail.com>
2025-06-09 12:46:45 +0530

    updated ml-pipeline

---
+++ src/pipeline/ml_pipeline.py
@@ -0,0 +1,33 @@
+import sys
+from src.logger import logging
+from src.exception import AppException
+from src.components.data_ingestion import DataIngestion
+from src.components.data_validation import DataValidation
+
+class MLPipeline:
+    def __init__(self):
+        self.data_ingestion = DataIngestion()
+        self.data_validation = DataValidation()
+    
+    def main(self):
+        try:
+            logging.info("STAGE:1 Data ingestion stage initiated")
+            self.data_ingestion.initiate_data_ingestion()
+
+            logging.info("STAGE:2 Data validation stage initiated")
+            self.data_validation.initiate_data_vatidation()
+        
+        except Exception as e:
+            logging.error(f"ML Pipeline Terminated: {e}", exc_info=True)
+            raise AppException(e, sys)
+        
+if __name__=='__main__':
+    try:
+        logging.info("Starting ML Pipeline")
+        obj = MLPipeline()
+        obj.main()
+        logging.info("ML Pipeline completed successfully.")
+        
+    except Exception as e:
+        logging.error(f"ML pipeline terminated: {e}")
+        raise AppException(e, sys)
